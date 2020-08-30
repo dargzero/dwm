1b08d7a081b388728e540d91c90abe87731e4a5b
Richard Garai <dargzero@gmail.com>
2020-08-30 10:08:25 +0200

    removed gimp wm rule

--- config.h
+++ config.h
@@ -35,10 +35,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class        instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",       NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "alacritty",  NULL,     NULL,           0,         0,          1,          -1,        -1 },
-	{ NULL,         NULL,     "Event Tester", 0,         1,          0,           1,        -1 }, /* xev */
+	/* class        instance  title               tags mask  isfloating  isterminal  noswallow  monitor */
+	{ NULL,         NULL,     "Android Emulator", 0,         1,          0,           0,        -1 },
+	{ "alacritty",  NULL,     NULL,               0,         0,          1,          -1,        -1 },
+	{ NULL,         NULL,     "Event Tester",     0,         1,          0,           1,        -1 }, 
 };
 
 /* layout(s) */
