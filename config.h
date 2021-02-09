7e5a438c14a7f01d39a5f9e64cfcc0a926405f22
Richard Garai <garric@okki.hu>
2021-02-09 17:51:56 +0100

    font awesome 5 icon updates

--- config.h
+++ config.h
@@ -8,7 +8,10 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Noto Sans Display:size=11" };
+static const char *fonts[]          = { 
+	"Noto Sans Display:size=11",
+	"FontAwesome:size=11"
+};
 static const char dmenufont[]       = "Noto Sans Display:size=11";
 
 static const char col_gray[]  = "#373737";
